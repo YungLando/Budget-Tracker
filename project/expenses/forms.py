a3ef959250c9bf46c86b1497bfa6269b0e9ff89e
Yunglando <landonrussell00@gmail.com>
2024-06-08 02:52:19 -0400

    finished setup and created basic dashboard and login page

---
+++ project/expenses/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Income, Expense
+
+class IncomeForm(forms.ModelForm):
+    class Meta:
+        model = Income
+        fields = ['source', 'amount', 'date']
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ['category', 'description', 'amount', 'date']
+
